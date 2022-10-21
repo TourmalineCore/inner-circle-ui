307343308074315080967c469c7bea357fe4508c
Anny <annet.f1limonova@yandex.ru>
2022-10-21 09:58:03 +0300

    refactor: #31ew8mx: delete-auth-part

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,12 @@ import {
   Route,
 } from 'react-router-dom';
 
-import LoginPage from './features/auth/LoginPage';
-import LogoutPage from './features/logout/LogoutPage';
-import Template from './template/Template';
-import { authService } from './common/authService';
-
-const WithPrivateRoute = authService.withPrivateRoute(Template);
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<LoginPage />} />
-        <Route path="/logout" element={<LogoutPage />} />
         <Route
           path="/*"
-          element={<WithPrivateRoute />}
         />
       </Routes>
     </BrowserRouter>
