9c9440a371fea6e52bab0a3ac59a17241a053ae8
Anny <102854214+KAA20Work@users.noreply.github.com>
2022-10-21 18:06:59 +0300

    fix(app): #31ew8mx: the home page is now displayed

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
+import { withPrivateRoute } from './routes/authStateProvider/withPrivateRoute';
+import Template from './template/Template';
+
+const WithPrivateRoute = withPrivateRoute(Template);
 
 export default function App() {
+
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route
-          path="/*"
-        />
-      </Routes>
+            path="/*"
+            element={<WithPrivateRoute />}
+          />
+        </Routes>
     </BrowserRouter>
   );
 }
