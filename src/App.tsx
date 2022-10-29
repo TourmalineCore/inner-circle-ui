5dfe87b7c3deda74ba27f8e0db3e9b7046c6ab9d
chore(auth): #319b266: add auth package, configuredYasnovsky <andrey5.1997@mail.ru>
2022-10-29 14:05:28 +0500

    refactor(app): #31ew8mx: margins and spaces eslint

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,14 @@ import Template from './template/Template';
 const WithPrivateRoute = withPrivateRoute(Template);
 
 export default function App() {
-
-  
   return (
     <BrowserRouter>
       <Routes>
         <Route
-            path="/*"
-            element={<WithPrivateRoute />}
-          />
-        </Routes>
+          path="/*"
+          element={<WithPrivateRoute />}
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
