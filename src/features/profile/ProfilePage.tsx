fd5338703aa7eb3d176ca8396768092af220992a
littlebomb <littlebomb86@gmail.com>
2022-11-05 12:00:01 +0500

    feat: #32wmbfc: add data to profile page

--- src/features/profile/ProfilePage.tsx
+++ src/features/profile/ProfilePage.tsx
@@ -8,8 +8,11 @@ import ProfileHeader from './components/ProfileHeader/ProfileHeader';
 import ProfileNav from './components/ProfileNav/ProfileNav';
 import getProfileAvailableActionButtons from './profileActionsFactory';
 
+import './styles/ProfilePage.scss';
+
 import { profileModes } from './profileModes';
 import { profileSections, profileTabs } from './profileTabs';
+import { infoData } from './mockData/infoData';
 
 function ProfilePage() {
   const [profileMode, setProfileMode] = useState(profileModes.VIEW);
@@ -18,7 +21,7 @@ function ProfilePage() {
 
   const activeTabKey = params.tabId || profileSections.SUMMARY;
 
-  const ActiveTab = profileTabs[activeTabKey].component;
+  const mockData = infoData;
 
   return (
     <ContentCard
@@ -55,12 +58,111 @@ function ProfilePage() {
       )}
     >
       <div style={{ height: 2000, backgroundColor: '#f8fcff' }}>
-        <br />
-        <ActiveTab />
-        <br />
-        mode:
-        {' '}
-        {profileMode}
+        <ul className="profile-data">
+          <li>
+            <b>Имя:</b>
+            {' '}
+            {mockData.name}
+          </li>
+          <li>
+            <b>Фамилия:</b>
+            {' '}
+            {mockData.surname}
+          </li>
+          <li>
+            <b>Электронная почта:</b>
+            {' '}
+            {mockData.email}
+          </li>
+          <li>
+            <b>Ставка в час:</b>
+            {' '}
+            {mockData.ratePerHour}
+            {' '}
+            рублей
+          </li>
+          <li>
+            <b>Полный оклад:</b>
+            {' '}
+            {mockData.fullSalary}
+            {' '}
+            рублей
+          </li>
+          <li>
+            <b>Ставка:</b>
+            {' '}
+            {mockData.employmentType}
+          </li>
+          <li>
+            <b>Оклад:</b>
+            {' '}
+            {mockData.salary}
+            {' '}
+            рублей
+          </li>
+          <li>
+            <b>Фактическая стоимость часа:</b>
+            {' '}
+            {mockData.hourCostFact}
+            {' '}
+            рублей
+          </li>
+          <li>
+            <b>Стоимость часа на руки:</b>
+            {' '}
+            {mockData.hourCostForHands}
+            {' '}
+            рублей
+          </li>
+          <li>
+            <b>Аванс:</b>
+            {' '}
+            {mockData.advancePayment}
+            {' '}
+            рублей
+          </li>
+          <li>
+            <b>Доход:</b>
+            {' '}
+            {mockData.income}
+            {' '}
+            рублей
+          </li>
+          <li>
+            <b>Расход:</b>
+            {' '}
+            {mockData.expenses}
+            {' '}
+            рублей
+          </li>
+          <li>
+            <b>Прибыль:</b>
+            {' '}
+            {mockData.profit}
+            {' '}
+            рублей
+          </li>
+          <li>
+            <b>Рентабельность:</b>
+            {' '}
+            {mockData.profitability}
+            %
+          </li>
+          <li>
+            <b>Зарплата до вычета НДФЛ:</b>
+            {' '}
+            {mockData.salaryBeforeTax}
+            {' '}
+            рублей
+          </li>
+          <li>
+            <b>Зарплата с вычетом НДФЛ:</b>
+            {' '}
+            {mockData.salaryAfterTax}
+            {' '}
+            рублей
+          </li>
+        </ul>
       </div>
     </ContentCard>
   );
--- src/features/profile/types/Profile.d.ts
+++ src/features/profile/types/Profile.d.ts
@@ -7,3 +7,23 @@ type ProfileNavItems = {
   onClick?: () => unknown;
   active?: boolean;
 };
+
+type InfoData = {
+  id: number;
+  name: string;
+  surname: string;
+  email: string;
+  ratePerHour: number;
+  fullSalary: number;
+  employmentType: number;
+  salary: number;
+  hourCostFact: number;
+  hourCostForHands: number;
+  advancePayment: number;
+  income: number;
+  expenses: number;
+  profit: number;
+  profitability: number;
+  salaryBeforeTax: number;
+  salaryAfterTax: number;
+};
