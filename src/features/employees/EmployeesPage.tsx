167e3d5d4d4fc45ead9bbf5b2d772112b661bae4
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-11-10 22:28:58 +0500

    fix(employee): #3381j1d: add a request to the back to get employee data

--- src/features/employees/EmployeesPage.tsx
+++ src/features/employees/EmployeesPage.tsx
@@ -1,16 +1,41 @@
 import { ClientTable } from '@tourmalinecore/react-table-responsive';
+import axios from 'axios';
+import {
+  useState,
+  useEffect,
+} from 'react';
 import ContentCard from '../../components/ContentCard/ContentCard';
 import DefaultCardHeader from '../../components/DefaultCardHeader/DefaultCardHeader';
+import EmployeeProps from './employeesData';
 
-import {
-  data,
-} from './employeesData';
+const QUOTE_SERVICE_URL = 'http://localhost:5000/api/employees/list';
 
 type Row<Type> = {
   original: Type
 };
 
 function EmployeesPage() {
+  const [employees, employeesState] = useState<EmployeeProps[]>([]);
+  function fetchQuotes() {
+    axios.get<EmployeeProps[]>(QUOTE_SERVICE_URL, {
+      method: 'GET',
+    }).then((response) => {
+      for (let i = 0; i < response.data.length; i++) {
+        response.data[i].fullname = `${response.data[i].name} ${response.data[i].surname}`;
+        if (!response.data[i].telegram || response.data[i].telegram?.length === 0) {
+          response.data[i].telegram = 'Not specified';
+        }
+      }
+      employeesState(response.data);
+    });
+  }
+  useEffect(() => {
+    try {
+      fetchQuotes();
+    } catch (err) {
+      console.log(err);
+    }
+  }, [employeesState]);
   return (
     <ContentCard
       isStickyHead
@@ -21,19 +46,19 @@ function EmployeesPage() {
       <div style={{ paddingTop: 4 }}>
         <ClientTable
           tableId="tc-story-bonus-table"
-          data={data}
+          data={employees}
           order={{
             id: 'weightForSorting',
             desc: true,
           }}
-          renderMobileTitle={(row : Row<{ employee: string }>) => row.original.employee}
+          renderMobileTitle={(row : Row<{ fullname: string }>) => row.original.fullname}
           enableTableStatePersistance
           maxStillMobileBreakpoint={1200}
           isStriped
           columns={[
             {
               Header: 'Employee',
-              accessor: 'fullName',
+              accessor: 'fullname',
               // Use our custom `fuzzyText` filter on this column
               filter: 'fuzzyText',
               nonMobileColumn: true,
@@ -44,6 +69,7 @@ function EmployeesPage() {
               accessor: 'workEmail',
               disableFilters: true,
               disableSortBy: true,
+              minWidth: 250,
             },
             {
               Header: 'Email',
