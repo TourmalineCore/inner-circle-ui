1c24dab1c9dd13dfcd888b8856413d3a17dcb964
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-11-15 15:17:18 +0500

    feat(payslips): #33e03g7: add search bar

---
+++ src/features/paySlips/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,11 @@
+import './SearchBar.css';
+
+function SearchBar() {
+  return (
+    <div className="searchbar">
+      <input className="searchbar-input" />
+    </div>
+  );
+}
+
+export default SearchBar;
