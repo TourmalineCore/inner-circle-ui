b17b8109c84c5291894702ab49045b86cd3df546
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-11-15 15:16:40 +0500

    refactor(payslips): #33e03g7: add data of employees

--- src/features/paySlips/PaySlipsPage.tsx
+++ src/features/paySlips/PaySlipsPage.tsx
@@ -1,8 +1,17 @@
 import PaySlips from './components/PaySlips/PaySlips';
+import SearchBar from './components/SearchBar/SearchBar';
+import './PaySlipsPage.css';
+
+const data = [{ name: 'people 1', date: '11-03-2022' }, { name: 'people 2', date: '11-03-2022' }];
 
 function PaySlipsPage() {
   return (
-    <PaySlips />
+    <div className="payslipspage">
+      <SearchBar />
+      {data.map((el) => (
+        <PaySlips columns={Object.entries(el)} />
+      ))}
+    </div>
   );
 }
 
--- src/features/paySlips/components/PaySlips/PaySlips.tsx
+++ src/features/paySlips/components/PaySlips/PaySlips.tsx
@@ -1,7 +1,20 @@
-function PaySlips() {
+import PayColumn from '../PayColumn/PayColumn';
+import './PaySlips.css';
+
+function PaySlips(
+  {
+    columns,
+  }:
+  {
+    columns: [string, string][]
+  },
+) {
   return (
-    <>
-    </>
+    <div className="payslips">
+      {columns.map((el) => (
+        <PayColumn name={el[1]} />
+      ))}
+    </div>
   );
 }
 
