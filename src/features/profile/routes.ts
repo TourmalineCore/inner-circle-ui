8dcf5b30924fb38370366ed9c6be1be980e2c611
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-11-16 17:06:39 +0500

    refactor(profile): #3380v02: change name of route

--- src/features/profile/routes.ts
+++ src/features/profile/routes.ts
@@ -6,7 +6,7 @@ import ProfilePage from './ProfilePage';
 export const profileRoutes = [
   {
     path: '/profile',
-    breadcrumb: 'Salary information',
+    breadcrumb: 'Profile',
     Component: ProfilePage,
   },
   {
@@ -19,7 +19,7 @@ export const profileRoutes = [
 export const profileSidebarRoutes = [
   {
     path: '/profile',
-    label: 'Salary',
+    label: 'Profile',
     icon: faUser,
   },
 ];
