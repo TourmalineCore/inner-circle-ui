8e060421154ca9f5085036fdf8b3582986732c31
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-11-17 14:04:09 +0500

    refactor(profilePage): #33e03g7: change profile page

--- src/features/profile/ProfilePage.tsx
+++ src/features/profile/ProfilePage.tsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import ContentCard from '../../components/ContentCard/ContentCard';
 import ProfileInfo from './components/ProfileInfo/ProfileInfo';
-// import { formatMoney } from '../../common/utils/formatMoney';
 import { Employee } from './types/Profile';
 import InfoComponent from './components/InfoComponent/InfoComponent';
 import ProfileButton from './components/ProfileButton/ProfileButton';
@@ -14,10 +13,9 @@ function ProfilePage() {
   const [employee, setEmployee] = useState<Employee>();
   const [isRedact, serIsRedact] = useState(false);
 
-  useEffect(() => { loadEmployeesAsync(); }, []);
-  const increment = () => {
-    serIsRedact(!isRedact);
-  };
+  useEffect(() => {
+    loadEmployeesAsync();
+  }, []);
 
   return (
     <ContentCard
@@ -27,73 +25,115 @@ function ProfilePage() {
         employee
           ? (
             <div className="profile">
-              <ProfileInfo rows={
-                [
-                  isRedact
-                    ? (
-                      <>
+              <ProfileInfo
+                rows={
+                  [
+                    isRedact
+                      ? (
+                        <>
+                          <InfoComponent
+                            value={`${employee.surname}`}
+                            isRedact={isRedact}
+                            label="Last Name"
+                          />
+                          <InfoComponent
+                            value={`${employee.name}`}
+                            isRedact={isRedact}
+                            label="First Name"
+                          />
+                          <InfoComponent
+                            value={`${employee.middleName}`}
+                            isRedact={isRedact}
+                            label="Middle Name"
+                          />
+                        </>
+                      )
+                      : (
                         <InfoComponent
-                          value={`${employee.surname}`}
-                          isRedact={isRedact}
+                          value={`${employee.surname} ${employee.name} ${employee.middleName}`}
                         />
-                        <InfoComponent
-                          value={`${employee.name}`}
-                          isRedact={isRedact}
+                      ),
+                    <InfoComponent
+                      value={`${employee.phone}`}
+                      isRedact={isRedact}
+                      label={isRedact ? 'Phone' : undefined}
+                    />,
+                    <InfoComponent
+                      value={`${employee.workEmail}`}
+                      isRedact={isRedact}
+                      label={isRedact ? 'Work Email' : undefined}
+                    />,
+                    <InfoComponent
+                      value={`${employee.personalEmail}`}
+                      isRedact={isRedact}
+                      label={isRedact ? 'Email' : undefined}
+                    />,
+                    <InfoComponent
+                      value={`${employee.telegram}`}
+                      isRedact={isRedact}
+                      label={isRedact ? 'Telegram' : undefined}
+                    />,
+                    <InfoComponent
+                      value={`${employee.skype}`}
+                      isRedact={isRedact}
+                      label={isRedact ? 'Skype' : undefined}
+                    />,
+                    <InfoComponent
+                      value={`${employee.netSalary}`}
+                      isRedact={isRedact}
+                      label={isRedact ? 'Salary' : undefined}
+                    />,
+                  ]
+                }
+                buttons={
+                  [
+                    isRedact
+                      ? (
+                        <>
+                          <ProfileButton
+                            value="Cancel"
+                            onClick={() => serIsRedact(!isRedact)}
+                          />
+                          <ProfileButton
+                            value="Send a reqiest to edit"
+                            onClick={() => serIsRedact(!isRedact)}
+                          />
+                        </>
+                      ) : (
+                        <ProfileButton
+                          value="Request to edit"
+                          onClick={() => serIsRedact(!isRedact)}
                         />
-                        <InfoComponent
-                          value={`${employee.middleName}`}
-                          isRedact={isRedact}
-                        />
-                      </>
-                    )
-                    : (
-                      <InfoComponent
-                        value={`${employee.surname} ${employee.name} ${employee.middleName}`}
-                        isRedact={isRedact}
-                      />
-                    ),
-                  <InfoComponent
-                    value={`${employee.phone}`}
-                    isRedact={isRedact}
-                  />,
-                  <InfoComponent
-                    value={`${employee.workEmail}`}
-                    isRedact={isRedact}
-                  />,
-                  <InfoComponent
-                    value={`${employee.personalEmail}`}
-                    isRedact={isRedact}
-                  />,
-                  <InfoComponent
-                    value={`${employee.telegram}`}
-                    isRedact={isRedact}
-                  />,
-                  <InfoComponent
-                    value={`${employee.skype}`}
-                    isRedact={isRedact}
-                  />,
-                  <InfoComponent
-                    value={`${employee.netSalary}`}
-                    isRedact={isRedact}
-                  />,
-                ]
-              }
-              />
-              <ProfileButton
-                value="Request to edit"
-                onClick={increment}
+                      ),
+                  ]
+                }
               />
             </div>
           )
           : null
       }
-
     </ContentCard>
   );
 
   async function loadEmployeesAsync() {
-    const { data } = await axios.get<Employee>(QUOTE_SERVICE_URL);
-    setEmployee(data);
+    try {
+      const { data } = await axios.get<Employee>(QUOTE_SERVICE_URL);
+      setEmployee(data);
+    } catch {
+      const datae : Employee = {
+        id: 1,
+        name: 'Антон',
+        surname: 'Антонов',
+        middleName: 'Антонович',
+        workEmail: 'anton@tourmaline.com',
+        personalEmail: 'anton@mail.ru',
+        phone: '+79128093630',
+        skype: '@anton.skype',
+        telegram: '@anton.telegram',
+        netSalary: 50000,
+      };
+      setEmployee(datae);
+    }
   }
 }
 
