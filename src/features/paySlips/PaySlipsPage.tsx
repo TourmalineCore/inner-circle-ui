3f76a51fefab0892776f94b6b49b6b8c26fd92d9
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-11-21 10:33:16 +0500

    refactor(payslips): #33e03g7: change pay slips page

--- src/features/paySlips/PaySlipsPage.tsx
+++ src/features/paySlips/PaySlipsPage.tsx
@@ -1,16 +1,22 @@
+import PayComponent from './components/PayComponent/PayComponent';
 import PaySlips from './components/PaySlips/PaySlips';
-import SearchBar from './components/SearchBar/SearchBar';
 import './PaySlipsPage.css';
 
-const data = [{ name: 'people 1', date: '11-03-2022' }, { name: 'people 2', date: '11-03-2022' }];
+const data = [{ name: 'Антон Антонов Антонович', date: '11-03-2022' }, { name: 'Антон Антонов Антонович', date: '11-03-2022' }];
 
 function PaySlipsPage() {
   return (
     <div className="payslipspage">
-      <SearchBar />
-      {data.map((el) => (
-        <PaySlips columns={Object.entries(el)} />
-      ))}
+      <PaySlips
+        columns={[
+          data.map((el) => (
+            <PayComponent
+              name={el.name}
+              date={el.date}
+            />
+          )),
+        ]}
+      />
     </div>
   );
 }
