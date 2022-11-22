1428b3682f6537cc3b6006283fe3e7cfb8acc2dd
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-11-23 00:27:59 +0500

    refactor(payslips): #33e03g7: add icon in search bar

--- src/features/paySlips/components/SearchBar/SearchBar.tsx
+++ src/features/paySlips/components/SearchBar/SearchBar.tsx
@@ -1,15 +1,24 @@
+import { IconProp, SizeProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import './SearchBar.css';
 
 function SearchBar({ startDate, setStartDate } : { startDate : Date | null, setStartDate: React.Dispatch<React.SetStateAction<Date | null>> }) {
   return (
-    <DatePicker
-      selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      fixedHeight
-      dateFormat="dd/MM/yyy"
-      placeholderText="dd/mm/yyyy"
-    />
+    <div className="search-bar">
+      <div className="search-bar-icon">
+        <FontAwesomeIcon size={'2xl' as SizeProp} icon={faCalendarAlt as IconProp} />
+      </div>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        fixedHeight
+        dateFormat="dd/MM/yyy"
+        placeholderText="dd/mm/yyyy"
+      />
+    </div>
   );
 }
 
