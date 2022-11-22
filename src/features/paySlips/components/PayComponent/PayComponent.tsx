d9b4a67386ea5b396b4b80aee53ddf8a5f167bd9
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-11-23 00:41:39 +0500

    refactor(payslips): #33e03g7: change link in pay component

--- src/features/paySlips/components/PayComponent/PayComponent.tsx
+++ src/features/paySlips/components/PayComponent/PayComponent.tsx
@@ -6,15 +6,17 @@ import { Link } from 'react-router-dom';
 
 function PayComponent({ id, name, date } : { id?: number, name?: string, date?: string }) {
   return (
-    <Link to={`/pay-slips/${id?.toString()}`}>
-      <div className="pay-component">
-        <div className="component-name">{name}</div>
-        <div className="component-date">{date}</div>
-        <button formMethod="get" className="component-download" type="button">
-          <FontAwesomeIcon size={'2xl' as SizeProp} icon={faFileDownload as IconProp} />
-        </button>
-      </div>
-    </Link>
+    <div className="pay-component">
+      <Link to={`${id}`}>
+        <div className="component-info">
+          <div className="component-name">{name}</div>
+          <div className="component-date">{date}</div>
+        </div>
+      </Link>
+      <button formMethod="get" className="component-download" type="button">
+        <FontAwesomeIcon size={'2xl' as SizeProp} icon={faFileDownload as IconProp} />
+      </button>
+    </div>
   );
 }
 
