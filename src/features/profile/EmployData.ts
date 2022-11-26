3c45ce5dc4a2d9c42c5be18423ebe2a3b1919073
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-11-26 23:29:58 +0500

    refactor(profile): #3380v02: add mock data

---
+++ src/features/profile/EmployData.ts
@@ -0,0 +1,25 @@
+import { Employee } from './types/Profile';
+
+export const datae : Employee = {
+  id: 1,
+  name: 'Антон',
+  surname: 'Антонов',
+  middleName: 'Антонович',
+  workEmail: 'anton@tourmaline.com',
+  personalEmail: 'anton@mail.ru',
+  phone: '+79568897630',
+  github: '@antonov',
+  gitlub: '@antonov',
+};
+
+export default function setData(newemp : Employee | undefined) {
+  datae.id = newemp?.id;
+  datae.name = newemp?.name;
+  datae.surname = newemp?.surname;
+  datae.middleName = newemp?.middleName;
+  datae.workEmail = newemp?.workEmail;
+  datae.personalEmail = newemp?.personalEmail;
+  datae.phone = newemp?.phone;
+  datae.github = newemp?.github;
+  datae.gitlub = newemp?.gitlub;
+}
