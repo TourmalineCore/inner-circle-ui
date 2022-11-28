8497189ec74ca4639d4a298c32c069057385650a
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-11-28 07:39:31 +0500

    refactor(employees): #3381j1d: add new routers for add employee page, edit contacts page, edit salary page

--- src/features/employees/routes.ts
+++ src/features/employees/routes.ts
@@ -1,12 +1,31 @@
 import { faUsers } from '@fortawesome/free-solid-svg-icons';
 
+// import { BreadcrumbComponentProps } from 'use-react-router-breadcrumbs';
 import EmployeesPage from './EmployeesPage';
+import EmployeeAddPage from './components/EmployeeAddPage/EmployeeAddPage';
+import EmployeeEditContactPage from './components/EmployeeEditContactPage/EmployeeEditContactPage';
+import EmployeeEditSalaryPage from './components/EmployeeEditSalaryPage/EmployeeEditSalaryPage';
 
 export const employeesRoutes = [
   {
     path: '/employees',
     breadcrumb: 'Employees',
     Component: EmployeesPage,
+  },
+  {
+    path: '/employees/add',
+    breadcrumb: 'Add an employee',
+    Component: EmployeeAddPage,
+  },
+  {
+    path: '/employees/edit-contact&:id',
+    breadcrumb: 'Edit contact details',
+    Component: EmployeeEditContactPage,
+  },
+  {
+    path: '/employees/edit-salary&:id',
+    breadcrumb: 'Edit salary data',
+    Component: EmployeeEditSalaryPage,
   },
 ];
 
