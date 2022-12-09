664b018871dede653d4ba3e209ab6cb4ba2d7f1e
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-12-09 22:41:47 +0500

    refactor(api): add token constant

--- src/common/api.ts
+++ src/common/api.ts
@@ -12,6 +12,7 @@ const createAPI = (): AxiosInstance => {
       const token = authService.getAuthToken();
 
       if (config.headers) {
+        // eslint-disable-next-line max-len
         config.headers.Authorization = token ? `Bearer ${token}` : 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJQZXJtaXNzaW9ucyI6WyJDYW5WaWV3RmluYW5jZUZvclBheXJvbGwiLCJDYW5WaWV3QW5hbHl0aWMiLCJDYW5NYW5hZ2VFbXBsb3llZXMiXSwiZXhwIjoxNjczMjgyOTY4fQ.AW6qMuOtOgKHkvXUG7DARY_gHZm063Jjn0Fs5eApxvaXiRO8_-zW4q9RUQpwzj0Z1IKszl8ygtlE_17uW47-LUifmjwwz_nbNCe1aaSwv6_MFpG5OPPQzt2lZOcQ5hTKhU3zE6MpjkdYjKcyrivnPZMx1NOlXTQTErboff96WKhwbXs2z0ZsJBgvoENHbinWtjABmkU5-G6J-MZCgLwI8OeRs0Dlavaqa1m90lXwyWzQrKIJ2XLKbAg9cjX6msxTYdo4jrnpUvDq9ofxgismrk0f5RW9u_ZJBkis7FMu3gqU8w0cy3hV5Tv38dfnfAdNEzP7VbZky5qwQQZ2rJ6GwQ';
       }
 
@@ -23,4 +24,3 @@ const createAPI = (): AxiosInstance => {
 };
 
 export const api = createAPI();
-// .Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJQZXJtaXNzaW9ucyI6WyJDYW5WaWV3RmluYW5jZUZvclBheXJvbGwiLCJDYW5WaWV3QW5hbHl0aWMiLCJDYW5NYW5hZ2VFbXBsb3llZXMiXSwiZXhwIjoxNjczMjgyOTY4fQ.AW6qMuOtOgKHkvXUG7DARY_gHZm063Jjn0Fs5eApxvaXiRO8_-zW4q9RUQpwzj0Z1IKszl8ygtlE_17uW47-LUifmjwwz_nbNCe1aaSwv6_MFpG5OPPQzt2lZOcQ5hTKhU3zE6MpjkdYjKcyrivnPZMx1NOlXTQTErboff96WKhwbXs2z0ZsJBgvoENHbinWtjABmkU5-G6J-MZCgLwI8OeRs0Dlavaqa1m90lXwyWzQrKIJ2XLKbAg9cjX6msxTYdo4jrnpUvDq9ofxgismrk0f5RW9u_ZJBkis7FMu3gqU8w0cy3hV5Tv38dfnfAdNEzP7VbZky5qwQQZ2rJ6GwQ
