166d257ad4c9d36c6a0b17b3bda4cd164b795ef2
FremyXS <70861067+FremyXS@users.noreply.github.com>
2022-12-11 14:31:58 +0500

    refactor(infoComponent): change name of classes and change the icon image

--- src/features/profile/components/InfoComponent/InfoComponent.tsx
+++ src/features/profile/components/InfoComponent/InfoComponent.tsx
@@ -7,44 +7,42 @@ function InfoComponent({
   value,
   name,
   label,
-  isRedact = false,
   onChange,
   faIcon,
   icon,
-  text,
 } :
 { value: string,
   name?: string
   label? : string,
-  isRedact?: boolean,
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void,
   faIcon? : IconDefinition,
-  icon? : string
-  text? : string
+  icon?: string
 }) {
   return (
-    <div className="info-component">
-      {isRedact
+    <div className="component">
+      {onChange
         ? (
           <Input
-            className="info-component-input"
             value={value}
             label={label}
             onChange={onChange}
             name={name}
           />
         ) : (
-          <div className="info-component-label">
+          <div className="component-info">
             {faIcon
-              ? (
-                <div className="component-label-faicon">
+              && (
+                <div className="component-info__faicon">
                   <FontAwesomeIcon size={'xl' as SizeProp} icon={faIcon as IconProp} />
                 </div>
-              ) : (
-                <div className={icon ? `component-label-icon ${icon}` : undefined} />
               )}
-
-            <div className={`component-label-value ${text}`}>{value}</div>
+            {icon
+              && (
+                <div className="component-info__icon">
+                  <img src={icon} height="35px" alt="icon" />
+                </div>
+              )}
+            <div className="component-info__value">{value}</div>
           </div>
         )}
     </div>
