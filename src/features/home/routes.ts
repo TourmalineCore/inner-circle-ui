4559d726f7d248381c23fe20378ebcad3110a807
chore(auth): #319b266: add auth package, configuredYasnovsky <andrey5.1997@mail.ru>
2022-12-27 10:10:47 +0500

    fix: add redirect with homepage to analytics

---
+++ src/features/home/routes.ts
@@ -0,0 +1,9 @@
+import HomePage from './HomePage';
+
+export const homeRoutes = [
+  {
+    path: '/',
+    breadcrumb: 'Homepage',
+    Component: HomePage,
+  },
+];
--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -3,12 +3,14 @@ import { profileRoutes, profileSidebarRoutes } from '../features/profile/routes'
 import { SidebarRoutesProps } from '../types';
 import { employeesRoutes, employeesSidebarRoutes } from '../features/employees/routes';
 import { analyticsRoutes, analyticsSidebarRoutes } from '../features/analytics/routes';
+import { homeRoutes } from '../features/home/routes';
 
 export const adminRoutes: {
   path: string;
   breadcrumb: string | ((props: BreadcrumbComponentProps) => string | undefined);
   Component: () => JSX.Element;
 }[] = [
+  ...homeRoutes,
   ...profileRoutes,
   ...analyticsRoutes,
   ...employeesRoutes,
