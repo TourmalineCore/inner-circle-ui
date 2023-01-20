91b2d528d07d872a5e7cdada320b4fd4ab28a04c
Iliandr123 <isapronov@tourmalinecore.com>
2023-01-20 16:21:45 +0500

    chore: configure configs for cypress workability

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -1,7 +1,9 @@
-// export const {
-//   ENV_KEY,
-//   API_ROOT,
-//   API_ROOT_AUTH,
-//   LINK_TO_SALARY_SERVICE,
-//   LINK_TO_ACCOUNT_SERVICE,
-// } = window.__ENV__;
+const env = window.__ENV__ ? window.__ENV__ : process.env;
+
+export const {
+  ENV_KEY,
+  API_ROOT,
+  API_ROOT_AUTH,
+  LINK_TO_SALARY_SERVICE,
+  LINK_TO_ACCOUNT_SERVICE,
+} = env;
