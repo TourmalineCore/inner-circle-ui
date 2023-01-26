071b8adc2f0160112dc3bfef42c3bdd16d0e313c
Iliandr123 <isapronov@tourmalinecore.com>
2023-01-26 16:22:13 +0500

    chore: remove cypress reporters for e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'cypress';
-import mochawesomeWriter from 'cypress-mochawesome-reporter/plugin';
 
 export default defineConfig({
   e2e: {
-    reporter: 'cypress-multi-reporters',
-    reporterOptions: {
-      configFile: 'reporterOptions.json',
-    },
     viewportWidth: 1200,
     viewportHeight: 660,
     specPattern: 'cypress/e2e/**/*.cy.js',
-    baseUrl: 'https://tourmalinecore.github.io/React-Admin-Template',
     video: false,
-    setupNodeEvents(on, config) {
-      mochawesomeWriter(on, config);
-    },
   },
 
   component: {
