849e6dd300ca740a412c09f47481d9eca6177181
Yasnovsky <andrey5.1997@mail.ru>
2023-05-03 11:42:52 +0500

    refactor: #862jp8nd9: prototype refactor profiles, remove profile edit and add function edit in to page

--- src/features/profile/components/InfoComponent/InfoComponent.tsx
+++ src/features/profile/components/InfoComponent/InfoComponent.tsx
@@ -1,17 +1,40 @@
-import ContactLink from '../../../../components/ContactLink/ContactLink';
+import { ReactNode } from 'react';
+import InfoTip from '../../../../components/InfoTip/InfoTip';
+import { ReactComponent as IconError } from '../../../../assets/icons/icon-error.svg';
 
-function InfoComponent({ value, icon, link }: { value: string | null, icon?:JSX.Element, link: string | null }) {
-  const linkValidity = link || undefined;
+function InfoComponent({
+  value,
+  icon,
+  label,
+  isError = false,
+}: {
+  label: string;
+  value: string | ReactNode,
+  icon?: ReactNode,
+  isError?: boolean;
+}) {
   return (
     <div className="info-component">
-      {icon && (
-        <div className="info-component__icon">
-          { icon }
+
+      <div className="info-component__card">
+        <div className="info-component__header">
+          <span className="info-component__name">{label}</span>
+          <span className="info-component__icon">{icon}</span>
         </div>
-      )}
-      <div className="info-component__value">
-        <ContactLink contact={value} link={linkValidity} />
+
+        <span className="info-component__value">
+          {value}
+        </span>
       </div>
+
+      {isError && (
+        <InfoTip
+          className="info-component__info-tip"
+          content="Please fill this field."
+          classNameContent="info-component__info-content"
+          icon={<IconError />}
+        />
+      )}
     </div>
   );
 }
