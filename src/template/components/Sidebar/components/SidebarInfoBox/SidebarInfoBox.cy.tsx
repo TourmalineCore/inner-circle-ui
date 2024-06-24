0baaa7f8c1a0fa9b72c0620d99e5738b970d4cbb
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-06-24 17:14:01 +0500

    refactor: refactor SidebarInfoBox.cy.tsx

--- src/template/components/Sidebar/components/SidebarInfoBox/SidebarInfoBox.cy.tsx
+++ src/template/components/Sidebar/components/SidebarInfoBox/SidebarInfoBox.cy.tsx
@@ -14,39 +14,45 @@ const initialDataThird = {
 
 describe('SidebarInfoBox', () => {
   it(`
-  GIVEN one of ui pages
+  GIVEN email starts at first letter of the name and then last name 
   WHEN open sidebar
-  THEN render user fullName
+  THEN render correct user fullName
   `, () => {
     mountComponent({
       infoBoxData: initialData,
     });
 
-    cy.getByData('sidebar-infobox').should('have.text', 'Lastname N.');
+    cy
+      .getByData('sidebar-infobox')
+      .should('have.text', 'Lastname N.');
   });
 
   it(`
-  GIVEN one of ui pages 
+  GIVEN exception email starts at ank and then last name without first letter
   WHEN open sidebar
-  THEN render user fullName
+  THEN render correct user fullName
   `, () => {
     mountComponent({
       infoBoxData: initialDataSecond,
     });
 
-    cy.getByData('sidebar-infobox').should('have.text', 'Klastname A.');
+    cy
+      .getByData('sidebar-infobox')
+      .should('have.text', 'Klastname A.');
   });
 
   it(`
-  GIVEN one of ui pages 
+  GIVEN exception email starts at yu and then last name 
   WHEN open sidebar
-  THEN render user fullName
+  THEN render correct user fullName
   `, () => {
     mountComponent({
       infoBoxData: initialDataThird,
     });
 
-    cy.getByData('sidebar-infobox').should('have.text', 'Lastname Y.');
+    cy
+      .getByData('sidebar-infobox')
+      .should('have.text', 'Lastname Y.');
   });
 });
 
