429b527b2014ccb82c99194d45074c82f2f75394
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-06-28 13:35:13 +0500

    test: EmployeesState.cy.ts
      GIVEN employees page
      WHEN called update filter
      THEN get value filter

--- src/features/employees/state/EmployeesState.cy.ts
+++ src/features/employees/state/EmployeesState.cy.ts
@@ -14,4 +14,22 @@ describe('EmployeesState', () => {
     ]);
     expect(employeesState.allEmployees).to.has.lengthOf(1);
   });
+
+  it(`
+  GIVEN employees page 
+  WHEN called update filter
+  THEN get value filter
+  `, () => {
+    employeesState.updateFilterTerm('all');
+    expect(employeesState.filterTerm).eq('all');
+
+    employeesState.updateFilterTerm('current');
+    expect(employeesState.filterTerm).eq('current');
+
+    employeesState.updateFilterTerm('fired');
+    expect(employeesState.filterTerm).eq('fired');
+
+    employeesState.updateFilterTerm('blank');
+    expect(employeesState.filterTerm).eq('blank');
+  });
 });
