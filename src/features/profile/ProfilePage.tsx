b02016c473b813280a39537ab1f1dd3c6bf79a69
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-07-02 11:48:08 +0500

    refactor: replace salary info to separate component

--- src/features/profile/ProfilePage.tsx
+++ src/features/profile/ProfilePage.tsx
@@ -2,18 +2,13 @@
 import { useEffect, useState } from 'react';
 
 import Skeleton from 'react-loading-skeleton';
-import { NumericFormat } from 'react-number-format';
 import { LINK_TO_SALARY_SERVICE } from '../../common/config/config';
 import { Employee } from './types/Profile';
 import { api } from '../../common/api';
 
-import { ReactComponent as IconBoxPercent } from '../../assets/icons/icon-box-percent.svg';
-import { ReactComponent as IconMoney } from '../../assets/icons/icon-money.svg';
-import { ReactComponent as IconPercent } from '../../assets/icons/icon-percent.svg';
-import { ReactComponent as IconVirginmoney } from '../../assets/icons/icon-virginmoney.svg';
-import { InfoCard } from './components/info-card/InfoCard';
 import { GeneralInfo } from './components/general-info/GeneralInfo';
 import { ContactsInfo } from './components/contacts-info/ContactsInfo';
+import { SalaryInfo } from './components/salary-info/SalaryInfo';
 
 const initialValues = {
   id: 0,
@@ -127,110 +122,14 @@ export function ProfilePage() {
         <div className="profile__box">
           <h2 className="profile__head">Salary</h2>
           {
-            isLoading && (
+            isLoading ? (
               <Skeleton
                 className="profile__skeleton"
                 count={4}
                 containerTestId="loading-salary"
               />
-            )
-          }
-          {
-            !isLoading && (
-              <div>
-                {
-                  employee.isSalaryInfoFilled
-                    ? (
-                      <>
-                        <InfoCard
-                          isHaveValue={employee.fullSalary > 0}
-                          value={(
-                            <NumericFormat
-                              type="text"
-                              displayType="text"
-                              value={employee.fullSalary}
-                              valueIsNumericString
-                              allowLeadingZeros
-                              thousandSeparator=","
-                              suffix=" ₽"
-                            />
-                          )}
-                          label="Full Salary"
-                          icon={<IconMoney />}
-                        />
-                        {
-                          employee.isEmployedOfficially && (
-                            <InfoCard
-                              isHaveValue={employee.districtCoefficient > 0}
-                              value={(
-                                <NumericFormat
-                                  type="text"
-                                  displayType="text"
-                                  value={employee.districtCoefficient}
-                                  valueIsNumericString
-                                  allowLeadingZeros
-                                  style={{
-                                    color: '#1ED400',
-                                  }}
-                                  prefix="+ "
-                                  thousandSeparator=","
-                                  suffix=" ₽"
-                                />
-                              )}
-                              label="District Coefficient (15 %)"
-                              icon={<IconPercent />}
-                            />
-                          )
-                        }
-                        {
-                          employee.isEmployedOfficially && (
-                            <InfoCard
-                              isHaveValue={employee.incomeTax > 0}
-                              value={(
-                                <NumericFormat
-                                  displayType="text"
-                                  value={employee.incomeTax}
-                                  valueIsNumericString
-                                  allowLeadingZeros
-                                  style={{
-                                    color: '#DA2228',
-                                  }}
-                                  prefix="- "
-                                  thousandSeparator=","
-                                  suffix=" ₽"
-                                />
-                              )}
-                              label="Income Tax (13 %)"
-                              icon={<IconBoxPercent />}
-                            />
-                          )
-                        }
-                        {
-                          employee.isEmployedOfficially && (
-                            <InfoCard
-                              isHaveValue={employee.netSalary > 0}
-                              value={(
-                                <NumericFormat
-                                  displayType="text"
-                                  value={employee.netSalary}
-                                  valueIsNumericString
-                                  thousandSeparator=","
-                                  suffix=" ₽"
-                                />
-                              )}
-                              label="Net Salary"
-                              icon={<IconVirginmoney />}
-                            />
-                          )
-                        }
-                      </>
-                    ) : (
-                      <span style={{ opacity: 0.5 }}>
-                        Your salary will be filled soon...
-                      </span>
-                    )
-                }
-              </div>
+            ) : (
+              <SalaryInfo employee={employee} />
             )
           }
         </div>
---
+++ src/features/profile/components/salary-info/SalaryInfo.tsx
@@ -0,0 +1,104 @@
+import { NumericFormat } from 'react-number-format';
+import { Employee } from '../../types/Profile';
+import { InfoCard } from '../info-card/InfoCard';
+import { ReactComponent as IconMoney } from '../../../../assets/icons/icon-money.svg';
+import { ReactComponent as IconPercent } from '../../../../assets/icons/icon-percent.svg';
+import { ReactComponent as IconBoxPercent } from '../../../../assets/icons/icon-box-percent.svg';
+import { ReactComponent as IconVirginmoney } from '../../../../assets/icons/icon-virginmoney.svg';
+
+export const SalaryInfo = ({
+  employee,
+} : {
+  employee: Employee
+}) => (
+  employee.isSalaryInfoFilled
+    ? (
+      <>
+        <InfoCard
+          isHaveValue={employee.fullSalary > 0}
+          value={(
+            <NumericFormat
+              type="text"
+              displayType="text"
+              value={employee.fullSalary}
+              valueIsNumericString
+              allowLeadingZeros
+              thousandSeparator=","
+              suffix=" ₽"
+            />
+          )}
+          label="Full Salary"
+          icon={<IconMoney />}
+        />
+        {
+          employee.isEmployedOfficially && (
+            <InfoCard
+              isHaveValue={employee.districtCoefficient > 0}
+              value={(
+                <NumericFormat
+                  type="text"
+                  displayType="text"
+                  value={employee.districtCoefficient}
+                  valueIsNumericString
+                  allowLeadingZeros
+                  style={{
+                    color: '#1ED400',
+                  }}
+                  prefix="+ "
+                  thousandSeparator=","
+                  suffix=" ₽"
+                />
+              )}
+              label="District Coefficient (15 %)"
+              icon={<IconPercent />}
+            />
+          )
+        }
+        {
+          employee.isEmployedOfficially && (
+            <InfoCard
+              isHaveValue={employee.incomeTax > 0}
+              value={(
+                <NumericFormat
+                  displayType="text"
+                  value={employee.incomeTax}
+                  valueIsNumericString
+                  allowLeadingZeros
+                  style={{
+                    color: '#DA2228',
+                  }}
+                  prefix="- "
+                  thousandSeparator=","
+                  suffix=" ₽"
+                />
+              )}
+              label="Income Tax (13 %)"
+              icon={<IconBoxPercent />}
+            />
+          )
+        }
+        {
+          employee.isEmployedOfficially && (
+            <InfoCard
+              isHaveValue={employee.netSalary > 0}
+              value={(
+                <NumericFormat
+                  displayType="text"
+                  value={employee.netSalary}
+                  valueIsNumericString
+                  thousandSeparator=","
+                  suffix=" ₽"
+                />
+              )}
+              label="Net Salary"
+              icon={<IconVirginmoney />}
+            />
+          )
+        }
+      </>
+    ) : (
+      <span style={{ opacity: 0.5 }}>
+        Your salary will be filled soon...
+      </span>
+    )
+);
