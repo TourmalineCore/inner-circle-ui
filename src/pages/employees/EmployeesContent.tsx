96e21984caba4a8e1d675f55affad176cdb97164
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-11-17 10:45:29 +0500

    feat: #103: add toast for notifications

--- src/pages/employees/EmployeesContent.tsx
+++ src/pages/employees/EmployeesContent.tsx
@@ -3,19 +3,33 @@ import './EmployeesContent.scss'
 import Avatar from '../../assets/images/avatar.png'
 
 import { observer } from "mobx-react-lite"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { hasAccessPermission } from "../../common/utils/tokenUtils"
 import { EmployeeList } from "./components/employee-list/EmployeeList"
 import { FilterMenu } from "../../components/filter-menu/FilterMenu"
 import { SearchBar } from "../../components/search-bar/SearchBar"
 import { EmployeesStateContext } from "./state/EmployeesStateContext"
 import { RadioSelect } from '../../components/radio-select/RadioSelect'
-// import { toast } from "react-toastify"
+import { toast } from "react-toastify"
 
 export const EmployeesContent = observer(() => {
   const employeesState = useContext(EmployeesStateContext)
   
   const employees = employeesState.allEmployees
+
+  useEffect(() => {
+    if (employeesState.isBlankEmployees) {
+      toast.warning(`You have blank employees. Please fill in their profiles.`, {
+        toastId: `blank-employees`,
+        position: `top-right`,
+        autoClose: false,
+        hideProgressBar: false,
+        closeOnClick: true,
+      })
+    }
+  }, [
+    employeesState.isBlankEmployees,
+  ])
 
   return (
     <section
@@ -25,17 +39,7 @@ export const EmployeesContent = observer(() => {
       {
         hasAccessPermission({
           permission: `ViewSalaryAndDocumentsData`, 
-        }) && <>
-          <FilterMenu />
-
-          {
-            // TODO: make as toast notification
-            employeesState.isBlankEmployees 
-              && <div className="employees__notification">
-                You have blank employees. Please fill in their profiles.
-              </div>
-          }
-        </>
+        }) && <FilterMenu />
       }
         
       <div className="employees__actions">
