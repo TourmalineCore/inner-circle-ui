189b6dcb0977575bb12e3306fac67de6fa0a15fa
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2025-11-17 09:00:49 +0500

    refactor: #103: refactoring employee types

--- src/types/employee.ts
+++ src/types/employee.ts
@@ -1,30 +1,22 @@
 import { Specialization } from "../common/constants/specializations"
 
-type BaseEmployee = {
+export type Employee = {
+  employeeId: number,
   fullName: string,
   corporateEmail: string,
-  personalEmail: string | null,
-  gitHub: string | null,
-  gitLab: string | null,
-}
-
-export type EditedEmployee = BaseEmployee & {
-  employeeId: number,
   specializations: Specialization[],
   birthDate: string | null,
   workerTime: string | null,
   phone: string | null,
-};
-
-export type Employee = EditedEmployee & {
   isCurrentEmployee: boolean,
   isBlankEmployee: boolean,
+  personalEmail: string | null,
+  gitHub: string | null,
+  gitLab: string | null,
 };
 
-export type EmployeeProfile = BaseEmployee & {
+export type EditedEmployee = Omit<Employee, 'isCurrentEmployee' | 'isBlankEmployee'>;
+
+export type EmployeeProfile = Omit<Employee, 'employeeId' | 'isCurrentEmployee' | 'isBlankEmployee'> & {
   id: number,
-  specializations: Specialization[],
-  birthDate: string,
-  workerTime: string | null,
-  phone: string | null,
 };
